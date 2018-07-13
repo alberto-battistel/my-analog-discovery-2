3a9430c059dba86b72d0acd99cc0e176507b8c8e
AlbertoB <alberto.battistel@rub.de>
2018-07-13 21:01:25 +0200

    Work with AnalogIn_Aquistion: runs in Py3. Try to understand buffer and number of samples.

--- samples/py/AnalogIn_Acquisition.py
+++ samples/py/AnalogIn_Acquisition.py
@@ -7,12 +7,17 @@
        Python 2.7, numpy, matplotlib
        python-dateutil, pyparsing
 """
+from __future__ import division
+from __future__ import print_function
+from builtins import str
+from past.utils import old_div
 from ctypes import *
 from dwfconstants import *
 import math
 import time
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
 
 if sys.platform.startswith("win"):
     dwf = cdll.dwf
@@ -29,20 +34,20 @@ rgdSamples = (c_double*4000)()
 #print DWF version
 version = create_string_buffer(16)
 dwf.FDwfGetVersion(version)
-print "DWF Version: "+version.value
+print("DWF Version: "+version.value.decode("utf-8"))
 
 #open device
-print "Opening first device"
+print("Opening first device")
 dwf.FDwfDeviceOpen(c_int(-1), byref(hdwf))
 
 if hdwf.value == hdwfNone.value:
     szerr = create_string_buffer(512)
     dwf.FDwfGetLastErrorMsg(szerr)
-    print szerr.value
-    print "failed to open device"
+    print(szerr.value.decode("utf-8"))
+    print("failed to open device")
     quit()
 
-print "Preparing to read sample..."
+print("Preparing to read sample...")
 
 #set up acquisition
 dwf.FDwfAnalogInFrequencySet(hdwf, c_double(20000000.0))
@@ -55,23 +60,23 @@ time.sleep(2)
 
 #begin acquisition
 dwf.FDwfAnalogInConfigure(hdwf, c_bool(False), c_bool(True))
-print "   waiting to finish"
+print("   waiting to finish")
 
 while True:
     dwf.FDwfAnalogInStatus(hdwf, c_int(1), byref(sts))
-    print "STS VAL: " + str(sts.value) + "STS DONE: " + str(DwfStateDone.value)
+    print("STS VAL: " + str(sts.value) + "STS DONE: " + str(DwfStateDone.value))
     if sts.value == DwfStateDone.value :
         break
     time.sleep(0.1)
-print "Acquisition finished"
+print("Acquisition finished")
 
 dwf.FDwfAnalogInStatusData(hdwf, 0, rgdSamples, 4000) # get channel 1 data
 #dwf.FDwfAnalogInStatusData(hdwf, 1, rgdSamples, 4000) # get channel 2 data
 dwf.FDwfDeviceCloseAll()
 
 #plot window
-dc = sum(rgdSamples)/len(rgdSamples)
-print "DC: "+str(dc)+"V"
+dc = old_div(sum(rgdSamples),len(rgdSamples))
+print("DC: "+str(dc)+"V")
 
 plt.plot(np.fromiter(rgdSamples, dtype = np.float))
 plt.show()
